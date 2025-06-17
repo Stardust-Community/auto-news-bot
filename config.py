6aa23bb90e2d6736cc4900c943b50a2aa871a14d
Stardust <arkasarkar235@gmail.com>
2025-06-17 21:01:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8132519140:AAHOHzEtIVjjDlBrI5AmpTHlZLfJZYJzCLs")
-CHAT_ID = os.environ.get("CHAT_ID", "YOUR_TELEGRAM_CHAT_ID")  # channel or group (use @username or numeric ID)
+CHAT_ID = os.environ.get("CHAT_ID", "-1002071747697")  # channel or group (use @username or numeric ID)
 
 FEEDS = [
     "https://www.animenewsnetwork.com/all/rss.xml",
