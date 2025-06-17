4c7a1631d7369f40364c3384ffab8e3e3d9058e6
Stardust <arkasarkar235@gmail.com>
2025-06-17 20:54:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "8132519140:AAHOHzEtIVjjDlBrI5AmpTHlZLfJZYJzCLs")
 CHAT_ID = os.environ.get("CHAT_ID", "YOUR_TELEGRAM_CHAT_ID")  # channel or group (use @username or numeric ID)
 
 FEEDS = [
